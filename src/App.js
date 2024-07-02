afac7a748c01e65db9151e139b101ab87159997b
sunilkumarsinghj <sunilworld1234ss@gmail.com>
2024-07-02 18:22:07 +0530

    added to the github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import './index2.css'
+import BTNmodule from './button.module.css';
+import { useState } from 'react';
+import { questions  } from './questions';
 
 function App() {
+ let[status,setStatus]=useState(false);
+ let[pstatus,setPstatus]=useState(false)
+ let[manuStatus,setManustatus]=useState(false);
+ let[modalShow,setModalShow]=useState(false);
+ let[showAns,setShowAns]=useState(questions[0].id)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+      <button className='en' onClick={()=>setModalShow(true)}>Enquiry now</button>
+      <div onClick={()=>setModalShow(false)}  className={`modalOverLay ${(modalShow)?'modalShow':''}`}></div>
+      <div className={`modalDiv ${(modalShow)?'modalSee':''}`}>
+        <h3>enquiry now <span onClick={()=>setModalShow(false)}>&times;</span></h3></div>
+      <button className='micon' onClick={()=>setManustatus(!manuStatus)}>
+        {(manuStatus)?<span>&times;</span>:<span>&#9776;</span>}
+        </button>
+      <div className={`manu ${(manuStatus)?'activeManu':''}`}>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>course</li>
+          <li>gellary</li>
+          <li>contact us</li>
+        </ul>
+      </div>
+    <input type={(pstatus)?'text':'password'} className={BTNmodule.inputt}/>
+    <button className={BTNmodule.warning} onClick={()=>setPstatus(!pstatus)}>{(pstatus)?'hide':'show'}</button><br/>
+      <button className='border' onClick={()=>setStatus(!status)}>
+        {(status) ?'HIDE !':'SHOW!'}
+      </button>
+      {(status)
+      ?
+      <p className='para'>welocome to react app !</p>
+       :
+       ''}
+       <br/>
+        <button className={BTNmodule.error}>module BTN</button>
+        <div>
+        {/*
+        <h1>Frequently Asked Questions(FAQs)</h1>
+        <div className='faqouter'>
+         {questions.map((faqItems,i)=>{
+          return(
+            <div className='faqItems'>
+            <h2 onClick={()=>setShowAns(faqItems.id)}>{faqItems.question}</h2>
+             <p className={(showAns==faqItems.id)?'activeAns':''}>{faqItems.answer}</p>
+             </div>
+  
+          )
+         })}                   
+        </div>*/}
+       </div> 
     </div>
+    
   );
 }
 
